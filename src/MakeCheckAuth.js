8804f2bc0a2acda54d0c21ab95d1db026bb70a39
minocys <harich.leung@gmail.com>
2016-05-30 12:02:09 +0800

    update checkauth edge cases

--- src/MakeCheckAuth.js
+++ src/MakeCheckAuth.js
@@ -2,26 +2,33 @@ import { verifyXSRF } from './xsrfHelpers';
 
 export default function MakeCheckAuth({ decodeJWT }, authRules) {
   return function(authRule) {
-    const token = decodeJWT(this.cookies.jwt);
-    const user = token.user;
-    const xsrfToken = this.headers.xsrf;
-  
-    const rule = authRule ? authRules[authRule] : () => true;
+
+    let token = {};
+    let user = {};
+
+    if (this.cookies.jwt) {
+      // Decode token
+      token = decodeJWT(this.cookies.jwt);
+      user = token.user;
+      
+      // Delete password and set user
+      delete user.password;
+      this.user = user;
+    }
+
+    // If no authentication rule is passed, return true
+    if (!authRule) {
+      return true;
+    }
+
+    const rule = authRules[authRule];
   
     // Throw error if authentication rule not pass in config
     if (!rule) {
       throw new Error(`Authentication rule ${authRule} not found.`)
     }
   
-    // Test XSRF and Authentication Rule
-    if (verifyXSRF(token.xsrfSecret, xsrfToken) && rule(user)) {
-      // Authentication passed, delete user password, return user data
-      delete user.password;
-      this.user = user;
-      return true;
-    } else {
-      // XSRF test failed
-      return false;
-    }
+    // Test Authentication Rule && XSRF
+    return rule(user) && verifyXSRF(token.xsrfSecret, this.headers.xsrf);
   }
 }
