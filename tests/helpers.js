33bbfbce8917dbf324ea4be242a5684c342745d6
minocys <harich.leung@gmail.com>
2016-07-05 23:09:04 -0400

    Add tests

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,24 +1,40 @@
 const users = {
-  1: { email: 'email@email.com', password: 'hello' },
-  2: { email: 'another@email.com', password: 'pizza' },
-  3: { email: 'thelast@email.com', password: 'late at night' }
+  1: { email: 'email@email.com', password: 'hello', role: 1 },
+  2: { email: 'another@email.com', password: 'pizza', role: 1 },
+  3: { email: 'thelast@email.com', password: 'late at night', role: 1 }
 };
 
-export function fetchUser(id) {
+export function fetchUser({ email }) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(users[id])
+      let user = {};
+      Object.keys(users).forEach(value => {
+        if ( users[value].email === email ) {
+          user = users[value];
+        }
+      });
+      resolve(user)
     }, 1000);
   })
 }
 
-export
-function createUser(user) {
+export function createUser(user) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const newId = Object.keys(users).length + 1;
-      users[newId] = user;
-      resolve(users[newId]);
+      user.role = 1;
+      users[newId] = Object.assign({}, user);
+      resolve(user);
     }, 1000)
   });
 }
+
+export const authRules = {
+  pass(user) {
+    return user.role === 1
+  },
+  
+  fail(user) {
+    return user.role === 2
+  }
+};
--- tests/testServer.js
+++ tests/testServer.js
@@ -1,21 +1,32 @@
 import Express from 'express';
-import { fetchUser, createUser } from './helpers';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import derelict from '../src';
+import { createUser, fetchUser, authRules } from './helpers';
 
-const testServer = Express();
+function setupServer(derelictConfig, done) {
+  const testServer = Express();
 
-derelict.setup({
+  testServer.use(cookieParser());
+  testServer.use(bodyParser.json());
 
-});
+  derelict.setup(derelictConfig);
 
-app.post('/login', derelict.logOut);
+  testServer.post('/login', derelict.logIn);
 
-app.post('/signup', derelict.signUp);
+  testServer.post('/signup', derelict.signUp);
 
-app.post('/logout', derelict.logOut);
+  testServer.post('/logout', derelict.logOut);
+  
+  testServer.get('/pass_auth', derelict.isAuth('pass'), (req, res) => {
+    res.sendStatus(200);
+  });
+  
+  testServer.get('/fail_auth', derelict.isAuth('fail'), (req, res) => {
+    res.sendStatus(200);
+  });
 
-const port = 3000;
+  return testServer;
+}
 
-export default testServer.listen(port, () => {
-  console.log(`Ready for testing on port ${port}`);
-});
+export default setupServer;
